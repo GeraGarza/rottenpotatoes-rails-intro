9434fc15e012b8c89c082fc7b5a165a926fc67cf
EC2 Default User <ec2-user@ip-172-31-33-192.us-east-2.compute.internal>
2020-02-08 02:57:18 +0000

    part 1 test.3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-
-  alias :std_redirect_to :redirect_to
-  def redirect_to(*args)
-  	flash.keep
-  	std_redirect_to *args
-  end
-  
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
 end
