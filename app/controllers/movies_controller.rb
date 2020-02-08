8e019ae15fd6d72b6998cb79b2d5809d5e9b4a4f
EC2 Default User <ec2-user@ip-172-31-33-192.us-east-2.compute.internal>
2020-02-08 04:33:58 +0000

    part 1 test2.95

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,7 +13,7 @@ class MoviesController < ApplicationController
   def index
     @all_ratings = Movie.ratings
     @indexSort = params[:indexSort] || session[:indexSort]
-    session[:ratings] = session[:ratings] || {'G'=>'', 'PG'=>'', 'PG-13'=>'', 'g'=>''}
+    session[:ratings] = session[:ratings] || {'G'=>'', 'PG'=>'', 'PG-13'=>'', 'R'=>''}
     @rate_param = params[:ratings] || session[:ratings]
     session[:indexSort] = @indexSort
     session[:ratings] = @rate_param
